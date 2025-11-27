87adf9ec7626b583a1528e5f97e92c8f070b8c71
liwia dzikuć <liwiaa7@gmail.com>
2025-11-27 21:03:11 +0100

    heurystyczny scraper przed ai i selenium + rotacja user-agentow

--- app/scraper.py
+++ app/scraper.py
@@ -3,15 +3,37 @@ import re
 import asyncio
 import httpx
 from openai import OpenAI
-
+import random
 from app.selenium_scraper import scrape_price_selenium
 
 API_KEY = os.getenv("OPENAI_API_KEY")
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+]
+def extract_price_regex_heuristic(html: str) -> float | None:
+    if not html:
+        return None
+    match = re.search(r'\d{1,4}[.,]\d{2}', html)
+    if match:
+        span_start, span_end = match.span()
+        context = html[max(0, span_start - 50): min(len(html), span_end + 50)].lower()
+        
+        if any(keyword in context for keyword in PRICE_KEYWORDS) or \
+           ('pln' in context or 'zł' in context or 'eur' in context or 'usd' in context):
+            return float(match.group(0).replace(",", "."))
+        
+        
+    return None
 
 async def scrape_raw_html(url: str) -> str | None:
+    headers = {
+        "User-Agent": random.choice(USER_AGENTS) 
+    }
     try:
         async with httpx.AsyncClient(timeout=15.0) as client:
-            response = await client.get(url, follow_redirects=True)
+            response = await client.get(url, follow_redirects=True,headers=headers)
             if response.status_code == 200:
                 return response.text
             return None
@@ -89,10 +111,14 @@ def scrape_price_ai(fragment: str) -> float | None:
         return None
 
 async def scrape_price_async(url: str) -> float | None:
-    print("\n--- AI SCRAPER START ---")
-
-    # 1 ai scraper
+    print("\n--- AI/HEURISTIC SCRAPER START ---")
     html = await scrape_raw_html(url)
+    price_regex = extract_price_regex_heuristic(html)
+    print("HEURISTIC REGEX RESULT:", price_regex)
+    if price_regex is not None:
+        print("--- HEURISTIC REGEX SUCCESS ---")
+        return price_regex
+    print("--- HEURISTIC SCRAPER FAILED, switching to AI ---")
     if html:
         fragment = extract_price_fragments(html)
         price_ai = scrape_price_ai(fragment)
@@ -105,10 +131,11 @@ async def scrape_price_async(url: str) -> float | None:
 
     print("--- AI SCRAPER FAILED, switching to SELENIUM ---")
 
-    # 2 selenium
+    # 3 selenium
     price_browser = scrape_price_selenium(url)
     print("SELENIUM RESULT:", price_browser)
     return price_browser
 
 def scrape_price(url: str) -> float | None:
     return asyncio.run(scrape_price_async(url))
+
--- app/selenium_scraper.py
+++ app/selenium_scraper.py
@@ -3,7 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 import re
+import random
 
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+]
 
 def scrape_price_selenium(url: str) -> float | None:
     print("=== SELENIUM START ===")
@@ -16,10 +22,8 @@ def scrape_price_selenium(url: str) -> float | None:
     options.add_argument("--window-size=1920,1080")
 
 
-    options.add_argument(
-        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"
-    )
+    random_user_agent = random.choice(USER_AGENTS)
+    options.add_argument(f"--user-agent={random_user_agent}")
 
     driver = uc.Chrome(options=options, headless=False)
 
